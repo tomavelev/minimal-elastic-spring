ec90abffe283877fee9bba426f7aedc9f40f2180
Toma Velev <tomavelev@gmail.com>
2024-10-08 12:49:30 +0300

    externalize config

--- src/main/java/com/programtom/elasticsearchapp/ElasticsearchConfig.java
+++ src/main/java/com/programtom/elasticsearchapp/ElasticsearchConfig.java
@@ -1,20 +1,31 @@
 package com.programtom.elasticsearchapp;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ElasticsearchConfig {
 
+    @Value("${elastic.host}")
+    String host;
+
+    @Value("${elastic.port}")
+    int port;
+
+    @Value("${elastic.scheme}")
+    String scheme;
+
+
     @Bean
     public ElasticsearchClient elasticsearchClient() {
         RestClient restClient = RestClient.builder(
-                new HttpHost("localhost", 9200, "http")).build();
+                new HttpHost(host, port, scheme)).build();
 
         // Create the transport with a Jackson mapper
         RestClientTransport transport = new RestClientTransport(
